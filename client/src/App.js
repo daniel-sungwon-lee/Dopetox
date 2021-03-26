77e9e2d5977f6c32085fa2ef05257c884a8ca209
Daniel Sungwon Lee <danlee333@gmail.com>
2021-03-25 21:15:55 -0700

    homepage logo

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/home';
 
 export default function App() {
   return (
     <div className="App">
+      <Router>
+        <Switch>
 
+          <Route exact path="/">
+            <Home />
+          </Route>
+
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ client/src/pages/home.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Home(props) {
+  return (
+    <div className="container">
+      <img src="images/dopetox.svg" width="300" alt="Dopetox logo" />
+    </div>
+  )
+}
