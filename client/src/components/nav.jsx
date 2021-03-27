5c743656bc786531a850a9d17a754598be357d88
Daniel Sungwon Lee <danlee333@gmail.com>
2021-03-26 18:07:10 -0700

    font update

--- client/src/components/nav.jsx
+++ client/src/components/nav.jsx
@@ -1,10 +1,18 @@
 import React, { useEffect, useState } from 'react';
-import { AppBar, Collapse, Toolbar } from '@material-ui/core';
+import { AppBar, Collapse, Toolbar, Tooltip, IconButton } from '@material-ui/core';
+import { NoteAddRounded } from '@material-ui/icons';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
   nav: {
     backgroundColor: "#FBDADE"
+  },
+  tooltip: {
+    backgroundColor: "#FB7B76"
+  },
+  icon: {
+    fontSize: "3rem",
+    color: "#FB7B76"
   }
 })
 
@@ -20,13 +28,21 @@ export default function Nav(props) {
     <Collapse in={show} timeout="auto">
       <div className="sticky-top mb-5">
         <AppBar position="sticky" classes={{root: classes.nav}}>
-          <Toolbar>
+          <Toolbar className="justify-content-between">
+
             <a href="/" className="text-decoration-none" onClick={() => props.setProgress('')}>
               <div className="d-flex align-items-center">
                 <img src="images/dopetox.svg" alt="Dopetox logo" width="40" />
                 <h3 className="mx-2 my-0 text-dark">Dopetox</h3>
               </div>
             </a>
+
+            <Tooltip title="Add" classes={{ tooltip: classes.tooltip }}>
+              <IconButton>
+                <NoteAddRounded className={classes.icon} />
+              </IconButton>
+            </Tooltip>
+
           </Toolbar>
         </AppBar>
       </div>
