3447264426a5ab31cda1f56753a951fbb9d517ee
Daniel Sungwon Lee <danlee333@gmail.com>
2021-03-29 16:16:19 -0700

    added timeago

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { CardContent, CardHeader, Paper } from '@material-ui/core';
+import TimeAgo from 'react-timeago';
 
 export default function Home(props) {
   const { setProgress } = props
@@ -30,15 +31,26 @@ export default function Home(props) {
             return (
               <Paper key={id} className="p-4 mb-5">
 
-                <CardHeader title={detox} subheader={username} />
-                <CardContent>
-                  <h5>Duration of detox:</h5>
-                  <h6>{duration}</h6>
-                  <h5>Game plan:</h5>
-                  <h6>{plan}</h6>
-                  <h5>Start date:</h5>
-                  <h6>{date}</h6>
-                </CardContent>
+                <div>
+                  <CardHeader title={detox} subheader={username} />
+                  <CardContent>
+                    <div className="mb-4">
+                      <h5>Duration of detox:</h5>
+                      <h6>{duration}</h6>
+                    </div>
+
+                    <div className="mb-4">
+                      <h5>Game plan:</h5>
+                      <h6>{plan}</h6>
+                    </div>
+
+                    <div>
+                      <h5>Start date:</h5>
+                      <h6 className="m-0">{date}</h6>
+                      <TimeAgo date={createdAt} style={{ color: "#FB7B76" }} />
+                    </div>
+                  </CardContent>
+                </div>
 
               </Paper>
             )
